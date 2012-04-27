a4826b15218a31f914020872a8ce994ee80413d9
Alex Luke <alexl@bnj.com>
2012-04-27 10:49:15 -0700

    Initial placeholder library

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-placeholder-polyfil
+Placeholder Polyfil
 ===================
 
 Add HTML5 placeholder support to older browsers
+
+Usage
+=====
+Use [Modernizr](http://modernizr.com/) to load this script for browsers that do not have native support
+
+```js
+Modernizr.load({
+	test: Modernizr.input.placeholder,
+	nope: 'placeholder.js'
+})
+```
---
+++ placeholder.js
@@ -0,0 +1,40 @@
+/*
+ * jQuery placeholder polyfil
+ * https://github.com/banduin/placeholder-polyfil
+ *
+ * Copyright (c) 2012 Alex Luke
+ * MIT Licensed
+ */
+;(function($) {
+	$(function() {
+		$('input[placeholder]').each(function() {
+			$this = $(this);
+			var ph = $this.prop('placeholder');
+			if (ph != '' && $this.val() == '') {
+				$this.val(ph);
+				$this.addClass('placeholder');
+				$this.focus(function() {
+					if ($(this).val() == ph) {
+						$(this).val('');
+						$(this).removeClass('placeholder');
+					}
+				});
+				$this.blur(function() {
+					if ($(this).val() == '') {
+						$(this).val(ph);
+						$(this).addClass('placeholder');
+					}
+				});
+			}
+		});
+		// Clear out placeholder values before submission
+		$('form:has(input[placeholder])').submit(function() {
+			$('input[placeholder]', this).each(function() {
+				$this = $(this);
+				var ph = $this.prop('placeholder');
+				if (ph != '' && $this.val() == ph)
+					$this.val('');
+			});
+		});
+	});
+})(jQuery);
